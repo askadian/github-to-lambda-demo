22aca51633adb5c62c235c25e4113a76205de5f0
Kadian <amkadian@us-glfy3yk9ff.lan>
2025-05-05 22:46:14 -0400

    test

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
-    print(('Done x1'))
+    print(('Done x1.1'))
     # This is a simple AWS Lambda function that returns a greeting message.
     #return {
     #    'statusCode': 200,
