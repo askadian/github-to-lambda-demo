6af06267d8f31b3031ff718817a782e074e08f54
Kadian <amkadian@us-glfy3yk9ff.lan>
2025-05-05 21:55:54 -0400

    added demo lambda code

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def lambda_handler(event, context):
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print(('Done x1'))
+    # This is a simple AWS Lambda function that returns a greeting message.
+    #return {
+    #    'statusCode': 200,
+    #    'body': 'Hello from AWS Lambda!'
+    #}
